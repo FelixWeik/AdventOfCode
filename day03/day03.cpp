d42845ee15d9ed97ad7c66fae290bb3813ec1cd5
Felix Weik <60046053+FelixWeik@users.noreply.github.com>
2024-12-03 20:01:57 +0100

    Update day03.cpp

--- day03/day03.cpp
+++ day03/day03.cpp
@@ -9,17 +9,10 @@ int main() {
     std::string input((std::istreambuf_iterator<char>(inputStream)), std::istreambuf_iterator<char>());
 
     std::regex pattern(R"(mul\((\d+),(\d+)\))");
-    std::vector<std::pair<int, int>> results;
 
     std::sregex_iterator begin(input.begin(), input.end(), pattern);
     std::sregex_iterator end;
-
-    for (auto it = begin; it != end; ++it) {
-        int x = std::stoi((*it)[1]);
-        int y = std::stoi((*it)[2]);
-        results.emplace_back(x, y);
-    }
-
+    
     int res = 0;
 
     for (auto it = begin; it != end; ++it) {
