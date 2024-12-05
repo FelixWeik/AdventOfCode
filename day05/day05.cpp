bb0bfa7a387a2e5db2fa7fc0c9c1fac0ade4de3c
Felix <uxhoe@student.kit.edu>
2024-12-05 13:25:14 +0100

    solved day05 task 1

---
+++ day05/day05.cpp
@@ -0,0 +1,92 @@
+#include "day05.hpp"
+
+using namespace std;
+
+unordered_map<int, unordered_set<int>> getRules(const vector<string>& rules) {
+    unordered_map<int, unordered_set<int>> precedence_map;
+    for (const string& rule : rules) {
+        int x, y;
+        char delim;
+        stringstream ss(rule);
+        ss >> x >> delim >> y;
+        precedence_map[x].insert(y);
+    }
+    return precedence_map;
+}
+
+bool isValidUpdate(const vector<int>& update, const unordered_map<int, unordered_set<int>>& precedence_map) {
+    unordered_map<int, int> position_map;
+    for (size_t i = 0; i < update.size(); ++i) {
+        position_map[update[i]] = i;
+    }
+
+    for (const auto& [x, y_set] : precedence_map) {
+        if (position_map.find(x) != position_map.end()) {
+            for (int y : y_set) {
+                if (position_map.find(y) != position_map.end() &&
+                    position_map[x] > position_map[y]) {
+                    return false;
+                }
+            }
+        }
+    }
+    return true;
+}
+
+int getMiddle(const vector<int>& update) {
+    return update[update.size() / 2];
+}
+
+vector<int> getUpdate(const string& line) {
+    vector<int> result;
+    stringstream ss(line);
+    string page;
+    while (getline(ss, page, ',')) {
+        result.push_back(stoi(page));
+    }
+    return result;
+}
+
+int main() {
+    string filename = "input05.txt";
+    ifstream input_file(filename);
+
+    if (!input_file.is_open()) {
+        cerr << "Fehler beim Öffnen der Datei: " << filename << endl;
+        return 1;
+    }
+
+    vector<string> rules;
+    vector<vector<int>> updates;
+    string line;
+    bool reading_updates = false;
+
+    while (getline(input_file, line)) {
+        if (line.empty()) {
+            reading_updates = true;
+            continue;
+        }
+
+        if (reading_updates) {
+            updates.push_back(getUpdate(line));
+        } else {
+            rules.push_back(line);
+        }
+    }
+
+    input_file.close();
+
+    unordered_map<int, unordered_set<int>> precedence_map = getRules(rules);
+
+    int res = 0;
+
+    for (const auto& update : updates) {
+        if (isValidUpdate(update, precedence_map)) {
+            res += getMiddle(update);
+        }
+    }
+
+    cout << "res: " << res << endl;
+
+    return 0;
+}
---
+++ day05/day05.hpp
@@ -0,0 +1,21 @@
+#ifndef __DAY05HPP__
+#define __DAY05HPP__
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <sstream>
+#include <algorithm>
+
+using namespace std;
+
+unordered_map<int, unordered_set<int>> getRules(const vector<string>& rules);
+bool isValidUpdate(const vector<int>& update, const unordered_map<int, unordered_set<int>>& precedence_map);
+int getMiddle(const vector<int>& update);
+vector<int> getUpdate(const string& line);
+int main();
+
+#endif
